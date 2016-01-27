64850105a09886e968573cc23f7163626b3d2393
zloykrivedka <zloykrivedka@gmail.com>
2016-01-27 05:44:12 +0300

    Накидал комментов :)

--- Project-M-end-M/src/js/scroll-bg.js
+++ Project-M-end-M/src/js/scroll-bg.js
@@ -1,4 +1,7 @@
 
+
+//Скрипт для эфекта увеличения при скроле
+//И эфекта длюра (говно, тормазит, потом переделаю)
 //$('.arkaplan').css('-webkit-filter', 'blur(5px)');
 $(window).scroll(function(e) {
     var scrollSeviye = $(this).scrollTop();
@@ -10,6 +13,13 @@ $(window).scroll(function(e) {
     var buyutme=(scrollSeviye/4000)+1;
 
     //$('.arkaplan').css('-webkit-filter', 'blur('+500/scrollSeviye+'px)');
-    $('.arkaplan').css('-webkit-filter', 'blur('+scrollSeviye/2400+'px)');
-    $('.arkaplan').css('transform', 'scale('+buyutme+')');
+    $('.bg-container').css('-webkit-filter', 'blur('+scrollSeviye/2400+'px)');
+    $('.bg-container').css('transform', 'scale('+buyutme+')');
+});
+
+//Эфент нажатия на иконку (первую), в обратную сторону потом сделаю)
+$(function() {
+    $('#iconStart').click(function (e) {
+        $.scrollTo('#endPage', 8000);
+    });
 });
